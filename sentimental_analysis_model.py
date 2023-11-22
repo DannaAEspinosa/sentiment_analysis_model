a3ca6067a629c7d54702e57d1c3ee6636efb40e8
Dan-Ale-Es-A <alexandraarenas1705@gmail.com>
2023-11-21 22:04:17 -0500

    fix_test_size_and_random_state_has_been_changed

--- sentimental_analysis_model.py
+++ sentimental_analysis_model.py
@@ -58,7 +58,7 @@ X = combined_df['Tokenized_Phrase']
 y = combined_df['tag']
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1234)
 
 #Create a DummyClassifier
 # Strategy: predict the most frequent class
